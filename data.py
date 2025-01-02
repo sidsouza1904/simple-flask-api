f25013b87c4bb08c564990e6090a35af29d8d4f0
Sidnei Souza <sidneilrsouza@gmail.com>
2025-01-02 16:12:08 -0300

    arquivo data.py deletado do projeto

--- data.py
+++
@@ -1,6 +0,0 @@
-students = {
-    1: {'name': 'Ana Maria', 'email': 'ana.maria@example.com', 'room': 1},
-    2: {'name': 'Maria Joana', 'email': 'maria.joana@example.com', 'room': 1},
-    3: {'name': 'João Pedro', 'email': 'joao.pedro@example.com', 'room': 2},
-    4: {'name': 'João Paulo', 'email': 'joao.paulo@example.com', 'room': 2},
-}
