0b66b702ddfb608e7a87efbbde894bf73903db2d
Sidnei Souza <sidneilrsouza@gmail.com>
2025-01-02 11:44:44 -0300

    Adicionada a rota para listar todos os estudantes

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask
+from data import students
 
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return 'Hello World!'
+@app.route('/students', methods=['GET'])
+def get():
+    return students
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000, debug=True)
